9aed8028693e7aabd300ac1492b719e1b5b18e8e
jaydhulia <jaydhulia@hotmail.com>
2021-01-17 12:41:19 -0700

    fix floorg

--- js/floor.js
+++ js/floor.js
@@ -1,17 +1,22 @@
 AFRAME.registerComponent('floor', {
   multiple: true,
     init: function(){
-      console.log("floor");
+      
     }
   });
   
-  AFRAME.registerComponent('camera-floor', {
-    dependencies: ['floor'],
-    init: function(){
-      var playerEl = this.el;
-      console.log("camera");
-      playerEl.setAttribute('look-controls', 'pointerLockEnabled: true');
-      playerEl.setAttribute('wasd-controls', '');
-      playerEl.setAttribute('kinematic-body', '');
-  }
+AFRAME.registerComponent('camera-floor', {
+  dependencies: ['floor'],
+  init: function(){
+    renderFloor();
+}
 });
+
+async function renderFloor(){
+  setTimeout(function() {
+    var playerEl = document.querySelector('[camera]');
+    playerEl.setAttribute('look-controls', 'pointerLockEnabled: true');
+    playerEl.setAttribute('wasd-controls', '');
+    playerEl.setAttribute('kinematic-body', '');
+  }, (4 * 1000));
+}
