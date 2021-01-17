19347e8da73d0e5b52b3b85a0194ce5173a30cb6
Shaihl Patel <shahilpatel@hotmail.com>
2021-01-17 01:55:04 -0500

    final stages done

--- js/keypad.js
+++ js/keypad.js
@@ -1,13 +1,24 @@
 AFRAME.registerComponent('my_keyboard_controller', {
     init: function(){
-        console.log("init here!!")
+        // console.log("init here!!")
+        // console.log(this.el)
       this.el.addEventListener('superkeyboardinput', function(event){
         // alert(detail.value); // the text
-        console.log(event.detail.value)
-      });
-      this.el.addEventListener('superkeyboarddismiss', function(event){
-        // alert(detail.value); // the text
-        console.log("dismissed")
-      });
+        if (event.detail.value === "26843") { 
+            // console.log(playerEl)
+            var playerEl = document.querySelector('[camera]');
+            var textOut = document.querySelector('#colored-text');
+            textOut.setAttribute("text", "value: Congratulation! You've been vaccinated and can attend HTN!");
+            textOut.setAttribute("text", "width: 6");
+            textOut.setAttribute("animation", "to: blue");
+            playerEl.setAttribute("look-controls","enabled:false");
+            playerEl.setAttribute("wasd-controls","enabled:false");
+            playerEl.setAttribute("position", { x: 0, y: 1.5, z: 0 });
+            playerEl.setAttribute("rotation", { x: 0, y: 0, z: 0 });
+        }
+        // console.log(event.detail.value)
+    });
     }
   });
+
+// I guess sandbox for users at the end lol
